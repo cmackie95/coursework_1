e9973d5010cd3aed94c2c1f05fff3a45aba0a1c9
cmackie95 <connormackie@yahoo.co.uk>
2019-11-01 16:54:55 +0000

    added else to if statement to deal with no parameters passed

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -18,7 +18,10 @@ class Dec2Hex
                 System.err.println("Argument" + args[0] + " must be an integer.");
                 System.exit(1);
             }
-        }
+        } else {
+			System.out.println("No value entered. Please enter an integer.");
+			System.exit(1);
+		}
 
     	char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
          int rem, num;
