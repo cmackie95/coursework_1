f17ce8f2de798fd63b728958a4d50fe58a32c1a5
cmackie95 <connormackie@yahoo.co.uk>
2019-11-06 10:48:21 +0000

    Allowed the program to be able to handle '0' as an input

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -8,46 +8,47 @@ public class Dec2Hex
     {
         Scanner scanner = new Scanner(System.in);
         String input = "";
-
-        if (args.length > 0) {
-			try {
+			if (args.length > 0) {
+				try {
 				Arg1 = Integer.parseInt(args[0]);
-			} 
-			catch (NumberFormatException e) {
+				} 
+				catch (NumberFormatException e) {
 				System.err.println("Argument '" + args[0] + "' must be an integer.");
+				}
 			}
-        }
 	
-        while (Arg1 == 0) {
-			System.out.println("");
-        	System.out.print("Enter value or enter 'q' to quit the program: ");
-          	input = scanner.nextLine();
-			if ("Q".equals(input) || "q".equals(input)) {
-				System.out.println("\nGoodbye.");
-				System.exit(1);
-			}
-      		try {
-                    Arg1 = Integer.parseInt(input);
-            } 
-            catch (NumberFormatException e) {
+			do {
+				System.out.println("");
+				System.out.print("Enter value or enter 'q' to quit the program: ");
+				input = scanner.nextLine();
+				if ("Q".equals(input) || "q".equals(input)) {
+					scanner.close();
+					System.out.println("\nGoodbye.");
+					System.exit(1);
+				}
+				try {
+					Arg1 = Integer.parseInt(input);
+				} 
+				catch (NumberFormatException e) {
                     System.err.println("Argument '" + input + "' must be an integer.");
-        	}
-        }
+				}
+			} while (Arg1 < 0);
 
 
-    	char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-        int rem, num;
-        num = Arg1;
-	    String hexadecimal=""; 
-        System.out.println("\nConverting the Decimal Value " + num + " to Hex...");
+			char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+			int rem, num;
+			num = Arg1;
+			String hexadecimal=""; 
+			System.out.println("\nConverting the Decimal Value " + num + " to Hex...");
         
-        while(num != 0) {
-			rem=num%16;
-			hexadecimal= ch[rem] + hexadecimal;
-            num= num/16;
-        }
-        
-        System.out.println("Hexadecimal representation is : " + hexadecimal);
-        scanner.close();
+			while(num != 0) {
+				rem=num%16;
+				hexadecimal= ch[rem] + hexadecimal;
+				num= num/16;
+			}
+			if (hexadecimal == "") {
+				hexadecimal = "0";
+			}
+			System.out.println("Hexadecimal representation is : " + hexadecimal);
     }
 }
